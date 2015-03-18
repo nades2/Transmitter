81983583cc2f0115de72c35cf37b43db5c82b976
nades2 <stephane.nadeau@gmail.com>
2015-03-17 20:38:27 -0400

    version 1
    transmitter with 433mhz module

---
+++ Tx_WeatherStation.ino
@@ -0,0 +1,173 @@
+
+//*-----( Import needed libraries )-----*/
+#include <dht11.h>
+#include <Wire.h>
+#include <SFE_BMP180.h>
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+
+/*-----( Declare objects )-----*/
+// set the LCD address to 0x27 for a 20 chars 4 line display
+// Set the pins on the I2C chip used for LCD connections:
+//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+dht11 DHT;
+SFE_BMP180 pressure;
+
+/*-----( Declare Variables )-----*/
+#define DHT11_PIN 2
+#define ALTITUDE 65.0
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  Serial.begin(9600);  // Used to type in characters
+  
+  // 433MHZ Device
+  Serial.println("setup Communication device");
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+  Serial.println("setup of Communication device completed");
+  
+  // Init of BMP180
+  Wire.begin();
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
+
+}
+
+void loop()
+{
+  //DHT11 read himidityv
+  int chk = DHT.read(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("DHT11: OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+  // put humidity in the variable
+  Serial.print("Humidity: ");
+  Serial.print(DHT.humidity);
+  Serial.print(" %");
+  // BMP180 section
+  
+  
+  char status;
+  double T,P,p0,a;
+
+  // Loop here getting pressure readings every 10 seconds.
+
+  // If you want sea-level-compensated pressure, as used in weather reports,
+  // you will need to know the altitude at which your measurements are taken.
+  // We're using a constant called ALTITUDE in this sketch:
+  
+  Serial.println();
+  Serial.print("provided altitude: ");
+  Serial.print(ALTITUDE,0);
+  Serial.print(" meters, ");
+  Serial.print(ALTITUDE*3.28084,0);
+  Serial.println(" feet");
+  
+  // If you want to measure altitude, and not pressure, you will instead need
+  // to provide a known baseline pressure. This is shown at the end of the sketch.
+
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.1,2);
+          Serial.println(" kPa");
+
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+
+  // Transmition section
+  // Envoyé la temperature en deux partis
+  char msg[24];
+  int tempC1 = (int)T;
+  double temp3 = (T - double(tempC1))*100; 
+  int tempC2 = (int)temp3;
+  // Envoyé la pression
+  int Pression = P;
+  Serial.println(Pression);
+  //Serial.println(Pression);
+  
+  sprintf(msg,"%i,%i,%i,%i",DHT.humidity,tempC1,tempC2,Pression);
+  vw_send((uint8_t *)msg, strlen(msg));
+  vw_wait_tx(); // Wait until the whole message is gone
+  delay(200);
+  
+  // end transmission section
+  
+  delay(1000);  // Pause for 5 seconds.
+}
